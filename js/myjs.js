3f488082aa8be7d4ccc67752639a9ba05eea4c58
HNDR2 <HNDR2>
2017-02-17 12:23:51 +0100

    definitivo1

--- js/myjs.js
+++ js/myjs.js
@@ -1,21 +1,21 @@
 function validaAA(valor, minValor, maxValor) {
-    return isNaN(valor) || valor < minValor || valor > maxValor ? false : valor;
+    if (isNaN(valor) || valor < minValor) return minValor;
+    if (valor > maxValor) return maxValor;
+    return valor;
 }
 
 
 function validaColor(color) {
-    if (color == "") return false;
-    else return color;
-    /* var $div = $("<div>");
-     $div.css("border", "1px solid " + color);
-     return ($div.css("border-color") != "")*/
+    let div0 = document.createElement("div");
+    div0.style.background = color;
+    return div0.style.background;
 }
 
 
 class Gauge {
     constructor(ancho, alto, texto, color1, color2) {
-        this.ancho = validaAA(ancho, 50, 2000) || 200;
-        this.alto = validaAA(alto, 50, 2000) || 200;
+        this.ancho = validaAA(ancho, 50, 2000);
+        this.alto = validaAA(alto, 50, 2000);
         this.texto = texto || "Parámetro";
         this.color1 = validaColor(color1) || "rgba(30% , 50%, 70%, .5)";
         this.color2 = validaColor(color2) || "rgba(90%, 25%, 20%, .8)";
@@ -27,21 +27,21 @@ class Gauge {
         this.divExterno.appendChild(this.divBase);
         // estilos div Externo
         this.divExterno.style.position = "absolute";
-        this.divExterno.style.width = ancho + "px";
-        this.divExterno.style.height = alto + "px";
+        this.divExterno.style.width = this.ancho + "px";
+        this.divExterno.style.height = this.alto + "px";
         this.divExterno.style.background = "linear-gradient(to left, " + this.color1 + " 50%, " + this.color2 + " 55% )";
         this.divExterno.style.borderRadius = "50%";
         // estilos div Interno
         this.divInterno.innerHTML = "50%";
         this.divInterno.style.position = "absolute";
-        this.divInterno.style.top = (alto * .075) + "px";
-        this.divInterno.style.left = (ancho * .05) + "px";
-        this.divInterno.style.width = (ancho * .85) + "px";
-        this.divInterno.style.height = (alto * .85) + "px";
+        this.divInterno.style.top = (this.alto * .075) + "px";
+        this.divInterno.style.left = (this.ancho * .05) + "px";
+        this.divInterno.style.width = (this.ancho * .85) + "px";
+        this.divInterno.style.height = (this.alto * .85) + "px";
         this.divInterno.style.borderRadius = "50%";
         this.divInterno.style.boxSizing = "border-box";
         this.divInterno.style.textAlign = "center";
-        this.divInterno.style.paddingTop = (alto * .1) + "px";//"35px";
+        this.divInterno.style.paddingTop = (this.alto * .1) + "px";//"35px";
         //------- Control de la transparencia del background del body
         let bk = getComputedStyle(document.body, null).backgroundColor;
         if (bk == "" || bk == "rgba(0, 0, 0, 0)" || bk == "transparent") bk = "white";
@@ -49,9 +49,9 @@ class Gauge {
         // estilos div Base
         this.divBase.innerHTML = this.texto;
         this.divBase.style.position = "absolute";
-        this.divBase.style.top = (alto * .4) + "px";
-        this.divBase.style.width = ancho + "px";
-        this.divBase.style.height = (alto * .6) + "px";
+        this.divBase.style.top = (this.alto * .4) + "px";
+        this.divBase.style.width = this.ancho + "px";
+        this.divBase.style.height = (this.alto * .6) + "px";
         this.divBase.style.background = bk;
         this.divBase.style.boxSizing = "border-box";
         this.divBase.style.textAlign = "center";
@@ -89,7 +89,7 @@ class Gauge {
 
 function inicializar() {
     let myDiv = document.getElementById("prueba");
-    let g1 = new Gauge(80, 100, "Valor", "transparent", "rgba(30%, 25%, 70%, .2)");
+    let g1 = new Gauge(100, 100, "Valor", "black", "33");//"rgba(30%, 25%, 70%, .2)");
     g1.ponGaugeIn(myDiv);
 
 
